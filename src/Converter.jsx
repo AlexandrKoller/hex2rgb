3f2959b57052cf69cb3edbef82b33a65dffb546f
Александр Коллер <aleksandrkoller@Noutbukleksandr.HomeLAN>
2025-05-16 19:15:48 +0300

    45

--- src/Converter.jsx
+++ src/Converter.jsx
@@ -3,6 +3,6 @@ import hexToRgba from "hex-to-rgba";
 export default function ConverterHexToRGB(hex) {
   if (hex.hex.length < 7 || hex.hex.length == 8) return;
   const regexp = /^#(([0-9A-Fa-f]{2}){3,4}|[0-9A-Fa-f]{3})$/;
-  if (!regexp.test(hex.hex)) return "#ОШИБКА!";
+  if (!regexp.test(hex.hex)) return <div>#ОШИБКА!</div>;
   return hexToRgba(hex.hex);
 }
